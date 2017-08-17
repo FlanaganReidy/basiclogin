d129f9ed2e087b68586d027efaa9752b4c4e6e3c
Shannon <reidy.pf@gmail.com>
2017-08-17 16:15:45 -0500

    will log in with a single username

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const parseurl = require('parseurl');
 const session = require('express-session');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-let logins = [{username:'kennyloggins', password: 'dangerzone'}];
+let logins = {username:'kennyloggins', password: 'dangerzone'};
 
 app.use(session({
   secret:'keyboard cat',
@@ -16,14 +17,26 @@ app.use(session({
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
-// app.use(function(req,res,next){
-//
-// })
-
-app.get('/login', function(req,res){
+app.get('/', function(req,res){
   res.render('login')
+})
+app.use(function(req, res, next){
+  let name = req.body.username;
+  let pword = req.body.password;
+    if(name === logins.username && pword === logins.password){
+      next();
+    }
+    else{
+      res.render('login');
+    }
+})
+app.post('/login', function(req, res){
+  console.log(req.body.username);
+  console.log(req.body.password);
+  res.render('home');
 })
 app.listen(3000, function(){
   console.log('Catcha raaaaaaiiiiiiiiide')
